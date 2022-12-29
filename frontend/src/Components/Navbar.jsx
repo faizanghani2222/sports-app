fcf9aa02649df3e5eafc4a88cc7ea88e2dc7da9f
Faizan Ghani <faizanghani2222@gmail.com>
2022-12-30 02:47:36 +0530

    completed sports-app backend

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <Box>
+            <Box>
+        
+            </Box>
+        </Box>
+    );
+}
+
+export default Navbar;
